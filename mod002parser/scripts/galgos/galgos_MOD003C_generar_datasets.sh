7e0917a9668e9b43e8db2bda2791ac7649202477
root <carlosandresgarcia1986@hotmail.com>
2018-01-25 00:53:11 +0100

    mas

--- mod002parser/scripts/galgos/galgos_MOD003C_generar_datasets.sh
+++ mod002parser/scripts/galgos/galgos_MOD003C_generar_datasets.sh
@@ -48,11 +48,69 @@ mysql -u root --password=datos1986 -t --execute="$CONSULTA_CON_IDs" >>$LOG_DS
 echo -e $(date +"%T")" Generando FEATURES-TRAINyTEST y TARGET-TRAINyTEST..."
 #Solo cogemos los galgos y carreras PASADAS (id_carrera es un valor grande, NO ES el inventado por mi)
 
+read -d '' CONSULTA_PASADO_SIN_IDs <<- EOF
+DROP TABLE IF EXISTS datos_desa.tb_features_pasado_sin_ids_${TAG};
+
+CREATE TABLE datos_desa.tb_features_pasado_sin_ids_${TAG} AS 
+SELECT 
+
+peso_galgo_norm, edad_en_dias_norm, scoring_remarks, experiencia, trap_factor, experiencia_en_clase, posicion_media_en_clase_por_experiencia, dif_peso, entrenador_posicion_norm, eed_norm, trap_norm, sp_norm,
+
+vgcortas_max_norm, vgmedias_max_norm, vglargas_max_norm, vel_real_cortas_mediana_norm, vel_real_cortas_max_norm, vel_going_cortas_mediana_norm, vel_going_cortas_max_norm, vel_real_longmedias_mediana_norm, vel_real_longmedias_max_norm, vel_going_longmedias_mediana_norm, vel_going_longmedias_max_norm, vel_real_largas_mediana_norm, vel_real_largas_max_norm, vel_going_largas_mediana_norm, vel_going_largas_max_norm,
+
+distancia_norm, num_galgos_norm, mes_norm, hora_norm, premio_primero_norm, premio_segundo_norm, premio_otros_norm, premio_total_carrera_norm, fc_1_norm, fc_2_norm, fc_pounds_norm, tc_1_norm, tc_2_norm, tc_3_norm, tc_pounds_norm, venue_going_std, venue_going_avg
+
+FROM datos_desa.tb_dataset_con_ids_${TAG}
+WHERE futuro=0
+;
+
+SELECT * FROM datos_desa.tb_features_pasado_sin_ids_${TAG} ORDER BY id_carrera LIMIT 5;
+SELECT count(*) as num_features_pasado_sin_ids FROM datos_desa.tb_features_pasado_sin_ids_${TAG} LIMIT 5;
+
+
+DROP TABLE IF EXISTS datos_desa.tb_target_pasado_sin_ids_${TAG};
+
+CREATE TABLE datos_desa.tb_target_pasado_sin_ids_${TAG} AS 
+SELECT TARGET FROM datos_desa.tb_dataset_con_ids_${TAG} WHERE futuro=0;
+
+SELECT * FROM datos_desa.tb_target_pasado_sin_ids_${TAG} ORDER BY id_carrera LIMIT 5;
+SELECT count(*) as num_targets_pasado_sin_ids FROM datos_desa.tb_target_pasado_sin_ids_${TAG} LIMIT 5;
+EOF
+
+echo -e $(date +"%T")"$CONSULTA_PASADO_SIN_IDs" 2>&1 1>>${LOG_DS}
+mysql -u root --password=datos1986 -t --execute="$CONSULTA_PASADO_SIN_IDs" >>$LOG_DS
+
+
 
 #### FUTURO: sin IDs ###
 echo -e $(date +"%T")" Generando FEATURES-PREDICCION (el modelo generara TARGET-PREDICCION)..."
 #Solo cogemos los galgos y carreras FUTURAS (id_carrera es un valor pequeño, inventado por mi)
 
+read -d '' CONSULTA_FUTURO_SIN_IDs <<- EOF
+DROP TABLE IF EXISTS datos_desa.tb_features_futuro_sin_ids_${TAG};
+
+CREATE TABLE datos_desa.tb_features_futuro_sin_ids_${TAG} AS 
+SELECT 
+
+peso_galgo_norm, edad_en_dias_norm, scoring_remarks, experiencia, trap_factor, experiencia_en_clase, posicion_media_en_clase_por_experiencia, dif_peso, entrenador_posicion_norm, eed_norm, trap_norm, sp_norm,
+
+vgcortas_max_norm, vgmedias_max_norm, vglargas_max_norm, vel_real_cortas_mediana_norm, vel_real_cortas_max_norm, vel_going_cortas_mediana_norm, vel_going_cortas_max_norm, vel_real_longmedias_mediana_norm, vel_real_longmedias_max_norm, vel_going_longmedias_mediana_norm, vel_going_longmedias_max_norm, vel_real_largas_mediana_norm, vel_real_largas_max_norm, vel_going_largas_mediana_norm, vel_going_largas_max_norm,
+
+distancia_norm, num_galgos_norm, mes_norm, hora_norm, premio_primero_norm, premio_segundo_norm, premio_otros_norm, premio_total_carrera_norm, fc_1_norm, fc_2_norm, fc_pounds_norm, tc_1_norm, tc_2_norm, tc_3_norm, tc_pounds_norm, venue_going_std, venue_going_avg
+
+FROM datos_desa.tb_dataset_con_ids_${TAG}
+WHERE futuro=1
+;
+
+SELECT * FROM datos_desa.tb_features_futuro_sin_ids_${TAG} ORDER BY id_carrera LIMIT 5;
+SELECT count(*) as num_features_pasado_sin_ids FROM datos_desa.tb_features_futuro_sin_ids_${TAG} LIMIT 5;
+EOF
+
+echo -e $(date +"%T")"$CONSULTA_PASADO_SIN_IDs" 2>&1 1>>${LOG_DS}
+mysql -u root --password=datos1986 -t --execute="$CONSULTA_PASADO_SIN_IDs" >>$LOG_DS
+
+
 echo -e $(date +"%T")" Generador de DATASETS (usando columnas elaboradas): FIN\n\n" 2>&1 1>>${LOG_DS}
+
 
 
