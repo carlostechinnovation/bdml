83634280c10327aec6aa371a3539f58aafb5f889
root <carlosandresgarcia1986@hotmail.com>
2018-02-10 11:47:47 +0100

    pruebas subgrupos

--- mod002parser/scripts/galgos/galgos_000_master.sh
+++ mod002parser/scripts/galgos/galgos_000_master.sh
@@ -11,20 +11,73 @@ rm -f $PATH_LOG
 
 
 
-
 ######## SUBGRUPOS #######################################################################
 function analizarScoreSobreSubgrupos ()
 {
-filtro_carreras="WHERE 1=1"
-filtro_galgos="WHERE 1=1"
-filtro_cg="WHERE 1=1"
-sufijo="SUBGRUPO_X"
+#filtro_carreras filtro_galgos filtro_cg sufijo
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "" "" "" "TOTAL"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "TOTAL" >>$PATH_LOG
 
 echo -e $(date +"%T")" --------" >>$PATH_LOG
-echo -e $(date +"%T")" $sufijo" >>$PATH_LOG
-${PATH_SCRIPTS}'galgos_MOD003C.sh' "$filtro_carreras" "$filtro_galgos" "$filtro_cg" "${sufijo}"
-${PATH_SCRIPTS}'galgos_MOD004.sh' "${sufijo}"
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "WHERE distancia_norm IS NULL OR distancia_norm <=0.33" "" "" "DISTANCIA_CORTA"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "DISTANCIA_CORTA" >>$PATH_LOG
+
 echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "WHERE distancia_norm IS NULL OR (distancia_norm >0.33 AND  distancia_norm <=0.66" "" "" "DISTANCIA_MEDIA"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "DISTANCIA_MEDIA" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "WHERE distancia_norm IS NULL OR distancia_norm >0.66" "" "" "DISTANCIA_LARGA"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "DISTANCIA_LARGA" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "WHERE hora_norm IS NULL OR hora_norm <=0.33" "" "" "HORA_PRONTO"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "HORA_PRONTO" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "WHERE hora_norm IS NULL OR hora_norm >=0.66" "" "" "HORA_TARDE"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "HORA_TARDE" >>$PATH_LOG
+
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "WHERE distancia_norm IS NULL OR distancia_norm >=0.66" "" "WHERE id_carrera IN ( SELECT DISTINCT id_carrera FROM datos_desa.tb_elaborada_carrerasgalgos_pre WHERE galgo_nombre IN (SELECT DISTINCT galgo_nombre FROM datos_desa.tb_elaborada_galgos_pre WHERE vel_going_largas_max_norm IS NULL OR vel_going_largas_max_norm<=0.33 ) )" "DISTANCIA_LARGA_Y_ALGUN_GALGO_MUY_LENTO"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "DISTANCIA_LARGA_Y_ALGUN_GALGO_MUY_LENTO" >>$PATH_LOG
+
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "" "" "WHERE edad_en_dias_norm is NULL OR edad_en_dias_norm<=0.33" "GALGOS_JOVENES"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "GALGOS_JOVENES" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "" "" "WHERE edad_en_dias_norm is NULL OR edad_en_dias_norm>=0.66" "GALGOS_VIEJOS"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "GALGOS_VIEJOS" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "" "" "WHERE experiencia_en_clase is NULL OR experiencia_en_clase<=0.33" "GALGOS_CON_POCA_EXPER_EN_CLASE"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "GALGOS_CON_POCA_EXPER_EN_CLASE" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "" "" "WHERE experiencia_en_clase is NULL OR experiencia_en_clase>=0.66" "GALGOS_CON_MUCHA_EXPER_EN_CLASE"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "GALGOS_CON_MUCHA_EXPER_EN_CLASE" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "" "" "WHERE peso_galgo_norm is NULL OR peso_galgo_norm<=0.33" "GALGOS_DELGADOS"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "GALGOS_DELGADOS" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "" "" "WHERE peso_galgo_norm is NULL OR peso_galgo_norm>=0.66" "GALGOS_GORDOS"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "GALGOS_GORDOS" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "" "" "WHERE entrenador_posicion_norm is NULL OR id_carrera IN ( SELECT DISTINCT id_carrera FROM datos_desa.tb_elaborada_carrerasgalgos_pre WHERE entrenador_posicion_norm>=0.5 )" "ALGUN_ENTRENADOR_CON_BUENOS_GALGOS"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "ALGUN_ENTRENADOR_CON_BUENOS_GALGOS" >>$PATH_LOG
+
+echo -e $(date +"%T")" --------" >>$PATH_LOG
+${PATH_SCRIPTS}'galgos_MOD003C.sh' "" "" "WHERE entrenador_posicion_norm is NULL OR id_carrera IN ( SELECT DISTINCT id_carrera FROM datos_desa.tb_elaborada_carrerasgalgos_pre WHERE entrenador_posicion_norm<0.5 )" "ALGUN_ENTRENADOR_CON_MALOS_GALGOS"
+${PATH_SCRIPTS}'galgos_MOD004.sh' "ALGUN_ENTRENADOR_CON_MALOS_GALGOS" >>$PATH_LOG
+
+
 }
 ##########################################################################################
 
@@ -57,7 +110,6 @@ filtro_carreras="WHERE 1=1"
 filtro_galgos="WHERE 1=1"
 filtro_cg="WHERE 1=1"
 sufijo="SUBGRUPO_X"
-
 #${PATH_SCRIPTS}'galgos_MOD003C.sh' "$filtro_carreras" "$filtro_galgos" "$filtro_cg" "${sufijo}"
 
 #echo -e $(date +"%T")" INTELIGENCIA ARTIFICIAL" >>$PATH_LOG
