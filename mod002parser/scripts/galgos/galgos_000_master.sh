8eb11d83fc784ff1fac0d9f4ccebcf46ecdfe152
root <carlosandresgarcia1986@hotmail.com>
2018-02-21 18:38:44 +0100

    Detectado ERROR en capa 040. Ver script analisis end-to-end

--- mod002parser/scripts/galgos/galgos_000_master.sh
+++ mod002parser/scripts/galgos/galgos_000_master.sh
@@ -20,18 +20,18 @@ echo -e "-------- "$(date +"%T")" ---------- GALGOS - Cadena de procesos -------
 echo -e "Ruta script="${PATH_SCRIPTS}
 echo -e "Ruta log (coordinador)="${PATH_LOG}
 
-echo -e $(date +"%T")" Descarga de datos (planificado con CRON)" >>$PATH_LOG
-${PATH_SCRIPTS}'galgos_MOD010.sh'
+#echo -e $(date +"%T")" Descarga de datos (planificado con CRON)" >>$PATH_LOG
+#${PATH_SCRIPTS}'galgos_MOD010.sh'
 
-echo -e $(date +"%T")" Analisis de datos: ESTADISTICA BASICA" >>$PATH_LOG
-${PATH_SCRIPTS}'galgos_MOD020.sh'
+#echo -e $(date +"%T")" Analisis de datos: ESTADISTICA BASICA" >>$PATH_LOG
+#${PATH_SCRIPTS}'galgos_MOD020.sh'
 
-echo -e $(date +"%T")" Generador de COLUMNAS ELABORADAS" >>$PATH_LOG
-${PATH_SCRIPTS}'galgos_MOD030.sh'
+#echo -e $(date +"%T")" Generador de COLUMNAS ELABORADAS" >>$PATH_LOG
+#${PATH_SCRIPTS}'galgos_MOD030.sh'
 
 
 #### Analisis de SUBGRUPOS ###
-analizarScoreSobreSubgrupos "$PATH_LOG"
+#analizarScoreSobreSubgrupos "$PATH_LOG"
 
 
 echo -e $(date +"%T")" DATASETS + FILTRADAS + INTELIGENCIA ARTIFICIAL" >>$PATH_LOG
@@ -43,9 +43,12 @@ ${PATH_SCRIPTS}'galgos_MOD040.sh' "TOTAL" >>$PATH_LOG
 
 #echo -e $(date +"%T")" Análisis posterior" >>$PATH_LOG
 #${PATH_SCRIPTS}'galgos_MOD060.sh'
+${PATH_SCRIPTS}'galgos_analisis_concreto_endtoend.sh'
 
 #echo -e $(date +"%T")" Análisis TIC de la ejecucion" >>$PATH_LOG
 #${PATH_SCRIPTS}'galgos_MOD007.sh'
+
+
 
 ##########################################
 
--- mod002parser/scripts/galgos/galgos_analisis_concreto_endtoend.sh
+++ mod002parser/scripts/galgos/galgos_analisis_concreto_endtoend.sh
@@ -8,8 +8,9 @@ FILE_TEMP="./temp_id_carrera_analisis"
 mysql -u root --password=datos1986 -N --execute="SELECT A.id_carrera FROM datos_desa.tb_dataset_ids_pasado_validation_TOTAL A LEFT JOIN (SELECT id_carrera FROM datos_desa.tb_filtrada_carrerasgalgos_TOTAL GROUP BY id_carrera HAVING count(*)=6) B ON A.id_carrera=B.id_carrera LIMIT 1;" > ${FILE_TEMP}
 id_carrera_analizada=$( cat ${FILE_TEMP})
 
+########################################################################################################
 
-read -d '' CONSULTA_ANALISIS_CARRERA <<- EOF
+read -d '' CONSULTA_ANTES_DE_PREDECIR <<- EOF
 SELECT id_carrera FROM datos_desa.tb_filtrada_carreras_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 10;
 
 SELECT A.galgo_nombre FROM datos_desa.tb_filtrada_galgos_TOTAL A WHERE A.galgo_nombre IN (SELECT galgo_nombre FROM datos_desa.tb_filtrada_carrerasgalgos_TOTAL WHERE id_carrera=${id_carrera_analizada} ) 
@@ -17,27 +18,40 @@ LIMIT 10;
 
 SELECT id_carrera, galgo_nombre, velocidad_con_going_norm FROM datos_desa.tb_filtrada_carrerasgalgos_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 10;
 
-SELECT id_carrera, galgo_nombre, futuro, vel_going_cortas_max_norm, vel_going_longmedias_max_norm, vel_going_largas_max_norm, distancia_norm, TARGET 
-FROM datos_desa.tb_ds_pasado_validation_featuresytarget_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 10;
+SELECT * FROM datos_desa.tb_ds_pasado_validation_featuresytarget_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 10;
+EOF
 
+echo -e "\nAntes de predecir:\n"
+echo -e "$CONSULTA_ANTES_DE_PREDECIR"
+mysql -u root --password=datos1986 -t --execute="$CONSULTA_ANTES_DE_PREDECIR"
 
+########################################################################################################
 
-
-
-
+read -d '' CONSULTA_DESPUES_DE_PREDECIR <<- EOF
+SELECT * FROM datos_desa.tb_val_TOTAL_aux1 LIMIT 6;
+SELECT * FROM datos_desa.tb_val_TOTAL_aux2 LIMIT 6;
+SELECT * FROM datos_desa.tb_val_TOTAL_aux3 LIMIT 6;
+SELECT * FROM datos_desa.tb_val_TOTAL_aux4 LIMIT 6;
+SELECT * FROM datos_desa.tb_val_TOTAL_aux5 WHERE id_carrera=${id_carrera_analizada} LIMIT 6;
 
 SELECT id_carrera, rowid, target_real, target_predicho FROM datos_desa.tb_val_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 10;
 
-select * FROM datos_desa.tb_score_aciertos_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 10;
+SELECT * FROM datos_desa.tb_val_score_real_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 6;
+SELECT * FROM datos_desa.tb_val_score_predicho_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 6;
+SELECT * FROM datos_desa.tb_score_aciertos_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 6;
+SELECT * FROM datos_desa.tb_val_connombre_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 6;
+SELECT * FROM datos_desa.tb_val_aciertos_connombre_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 6;
 
 select * FROM datos_desa.tb_val_economico_TOTAL WHERE id_carrera=${id_carrera_analizada} LIMIT 10;
 EOF
 
-echo -e "$CONSULTA_ANALISIS_CARRERA"
-mysql -u root --password=datos1986 -t --execute="$CONSULTA_ANALISIS_CARRERA"
+echo -e "\nDespues de predecir:\n"
+echo -e "$CONSULTA_DESPUES_DE_PREDECIR"
+mysql -u root --password=datos1986 -t --execute="$CONSULTA_DESPUES_DE_PREDECIR"
 
+########################################################################################################
 
-echo -e "ATENCION: debo que los galgos de ENTRADA sean los mismos que los de SALIDA y que tengan precio SP !!!!!!!!!"
+echo -e "\nATENCION: debo que los galgos de ENTRADA sean los mismos que los de SALIDA y que tengan precio SP !!!!!!!!!\n\n"
 
 
 
