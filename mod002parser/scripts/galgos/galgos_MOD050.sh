f1ca24f46ddeded615ab57f6705d15e777153b1c
root <carlosandresgarcia1986@hotmail.com>
2018-03-09 17:25:03 +0100

    Estable

--- mod002parser/scripts/galgos/galgos_MOD050.sh
+++ mod002parser/scripts/galgos/galgos_MOD050.sh
@@ -10,14 +10,9 @@ rm -f $LOG_050
 
 echo -e $(date +"%T")" Modulo 050 - PREDICCIÓN de carreras futuras: INICIO" 2>&1 1>>${LOG_050}
 echo -e "MOD050 --> LOG = "${LOG_050}
-<<<<<<< HEAD
+
 
 ########### Modelo predictivo REGRESION ###########
-=======
->>>>>>> branch 'master' of https://github.com/carlostechinnovation/bdml.git
-
-<<<<<<< HEAD
-
 PATH_MODELO_GANADOR='/home/carloslinux/Desktop/GIT_REPO_PYTHON_POC_ML/python_poc_ml/galgos/galgos_regresion_MEJOR_MODELO.pkl'
 
 echo -e $(date +"%T")" Ejecutando modelo (ya entrenado) sobre DS-FUTURO..." 2>&1 1>>${LOG_ML}
@@ -28,28 +23,12 @@ PATH_FILE_FUTURO_TARGETS="/home/carloslinux/Desktop/DATOS_LIMPIO/galgos/datos_de
 
 echo -e $(date +"%T")" Ejemplo de targets FUTUROS predichos:" 2>&1 1>>${LOG_ML}
 echo -e $(head -n 10 $PATH_FILE_FUTURO_TARGETS) 2>&1 1>>${LOG_ML}
-=======
-########### Modelo predictivo REGRESION ###########
->>>>>>> branch 'master' of https://github.com/carlostechinnovation/bdml.git
 
-
-
-
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> branch 'master' of https://github.com/carlostechinnovation/bdml.git
 ###################### MAIL ##########################
 #cat "$PATH_INFORME_FINAL" | mail -s "GALGOS - Prediccion carreras futuras Sportium" carlosandresgarcia1986@gmail.com,fcacereslau@hotmail.com,luisandresgarcia@gmail.com
 
 ##################################################
-<<<<<<< HEAD
-=======
 
 echo -e $(date +"%T")" Modulo 050 - PREDICCIÓN de carreras futuras: FIN" 2>&1 1>>${LOG_050}
->>>>>>> branch 'master' of https://github.com/carlostechinnovation/bdml.git
 
-echo -e $(date +"%T")" Modulo 050 - PREDICCIÓN de carreras futuras: FIN" 2>&1 1>>${LOG_050}
+
